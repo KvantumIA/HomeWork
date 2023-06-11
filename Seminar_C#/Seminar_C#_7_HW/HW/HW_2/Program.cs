63ebad7b4a9457e39c48a51c0d460245733eaf5d
Ivan <Koryakin.Ivan90@yandex.ru>
2023-06-11 22:41:46 +0900

    Делаю ДЗ 8

--- Seminar_C#/Seminar_C#_7_HW/HW/HW_2/Program.cs
+++ Seminar_C#/Seminar_C#_7_HW/HW/HW_2/Program.cs
@@ -15,23 +15,33 @@ void FillArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
-            array[i,j] = new Random().Next(1,15);
+            array[i,j] = new Random().Next(1,10);
 }
 
 void PrintArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
-        {
+    {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i,j],3}    ");
+            Console.Write($"{array[i,j],3}  ");
         System.Console.WriteLine();
-        }
+    }
 }
 
 void PrintPosition(int[] Position)
 {
-    foreach (var item in Position)
-        System.Console.Write($"{item}: ");
+    // int count = 0;
+    for (int i = 0; i < Position.Length; i++)
+    {
+        if (i == 0) System.Console.Write($"{Position[i]}:");
+        else if (i%2 == 1)
+            {
+                System.Console.Write($"{Position[i]};  ");
+            }
+        else if (i%2 == 0)
+            System.Console.Write($"{Position[i]}:");
+    }
+
 }
 
 bool Find(int[,] array, int num)
@@ -46,20 +56,39 @@ bool Find(int[,] array, int num)
     return Find;
 }
 
-int[] FindPosition(int[,] array, int num)
+int FindCount(int[,] array, int num)
 {
-    int[] Position = new int[2];
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (num == array[i,j])
+                {
+                    count = count + 2;
+                }
+            }
+        }
+    return count;
+}
+
+int[] FindPosition(int[,] array, int num, int count)
+{
+    int[] Position = new int[count];
+    int count2 = 0;
     for (int i = 0; i < array.GetLength(0); i++)
         {
         for (int j = 0; j < array.GetLength(1); j++)
             if (num == array[i,j])
-                {
-                    Position[0] = i;
-                    Position[1] = j;
-                }
+            {
+                Position[count2] = i;
+                Position[count2+1] = j;
+                count2 = count2 + 2;
+            }
         }
     return Position;
 }
+
 
 
 
@@ -70,11 +99,13 @@ System.Console.Write("Введите количество столбцов: ");
 int cols = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите число для поиска: ");
 int num = Convert.ToInt32(Console.ReadLine());
+
 int[,] array = new int [rows, cols];
 FillArray(array);
 PrintArray(array);
-int[] Position = FindPosition(array, num);
-
+int count = FindCount(array, num);
+int[] Position = FindPosition(array, num, count);
+System.Console.WriteLine();
 if (Find(array, num) == true)
     {
         System.Console.Write($"Такое число в массиве: есть. Его позиция = ");
